4a88d9c3db33152c4d1d74200422a92c6508edce
polaris <dinsor1545@gmail.com>
2025-12-04 13:48:47 +0700

    fix: add comments and initialize variables in exercises

--- ex-4.js
+++ ex-4.js
@@ -5,3 +5,5 @@ let numberOfSoldProduct = 12000;
 let numberOfProductInStock = undefined;
 
 console.log((numberOfSoldProduct / numberOfProductInStock) * 100);
+
+// output is Nan becasue 12000 divide by undefined so it will be return Nan
