c7bd72eb6a5f7d14a2783693be7252e54a161937
Q <eqmeomeo@hotmail.com>
2025-12-07 10:20:14 +0700

    feat : add console logs and comments for clarity in exercises

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,5 @@
 //Exercise #5: Shopping and Discount
-
-// Start coding here
+let productPrice = 27000;
+let discountPrice = (productPrice * 0.25); 
+let truePrice = productPrice - discountPrice;
+console.log(truePrice);
